6c448e9189defc7eff2a887c5fdb7a46a89617ee
hboehnke <hboehnke@cdd09686-bd49-0410-85b5-c69ef1c2b11f>
2008-12-06 20:30:49 +0000

    add varnames to strcode generator, move generated code to strcoded file

---
+++ src/genxml/code/genxml.cpp
@@ -0,0 +1,47 @@
+//   flexconf - flexible configuration generator
+//   Copyright (C) 2008 Holger Böhnke
+//
+//   This file is part of flexconf.
+//
+//   Flexconf is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//
+//
+/*stringname: pszHeaderProlog*/
+
+#ifndef _Struct_h_
+#define _Struct_h_
+
+#ifdef _MSC_VER
+  #define snprintf _snprintf
+  #pragma warning(disable: 4786 4996)
+#endif
+
+
+
+#include <string>
+#include <list>
+#include <set>
+#include <map>
+#include <stdio.h>
+
+
+
+/*stringname: pszSerProlog*/
+
+#include "struct.h"
+/*stringname: pszDesProlog*/
+
+#include "struct.h"
+#include <stdlib.h>
